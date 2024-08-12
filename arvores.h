eaa290e2313d1d6c67d29450718829bb0e6f1068
Samuel do Prado Rodrigues <samuelprado730@gmail.com>
2024-08-12 12:48:07 -0300

    fix: string.h warning

--- arvores.h
+++ arvores.h
@@ -1,20 +1,21 @@
-
+#include <string.h>
 typedef struct NO *arvAVL;
 typedef struct NO *arvLLRB;
 typedef struct {
-    char nome[50];
-    char empresa[50];
-    char departamento[50];
-    int codigo;
-    int idade;
-    float salario;
+  char nome[50];
+  char empresa[50];
+  char departamento[50];
+  int codigo;
+  int idade;
+  float salario;
 } myData;
 
-
-int insere_arvAVL(arvAVL *raiz, int valor, char nome[50], int idade, char empresa[50], char departamento[50], float salario);
+int insere_arvAVL(arvAVL *raiz, int valor, char nome[50], int idade,
+                  char empresa[50], char departamento[50], float salario);
 int remove_arvAVL(arvAVL *raiz, int valor);
 
-int insere_arvoreLLRB(arvLLRB *raiz, int valor, char nome[50], int idade, char empresa[50], char departamento[50], float salario);
+int insere_arvoreLLRB(arvLLRB *raiz, int valor, char nome[50], int idade,
+                      char empresa[50], char departamento[50], float salario);
 int remove_arvoreLLRB(arvLLRB *raiz, int valor);
 
 void confirmaInsercao(int x);
@@ -24,7 +25,3 @@ arvLLRB *cria_arvLLRB();
 
 void preOrdem_arvore(arvAVL *raiz);
 void preOrdem_arvore(arvLLRB *raiz);
-
-
-
-

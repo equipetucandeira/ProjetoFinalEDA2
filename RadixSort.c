21205df11bfdc3a66180fd65939fe34c27b17c6a
Samuel do Prado Rodrigues <samuelprado730@gmail.com>
2024-08-12 12:47:33 -0300

    refactor: myData support

--- RadixSort.c
+++ RadixSort.c
@@ -1,32 +1,31 @@
+#include "arvores.h"
 #include <assert.h>
 #include <errno.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
-#include "arvores.h"
 
 #define DECIMAL 10
 
 #define DIGIT(n, p) (n / p) % DECIMAL
 
-typedef int T;
+typedef myData T;
 
-int CountingSort(T *, const size_t,int);
+int CountingSort(T *, const size_t, int);
 
 int RadixSortLSD(T *, const size_t);
 
-static T GetMax(const T *array, const size_t size) {
-  T max = array[0];
+static int GetMax(const T *array, const size_t size) {
+  int max = array[0].codigo;
 
   for (size_t index = 0; index < size; ++index) {
-    if (max < array[index]) {
-      max = array[index];
+    if (max < array[index].codigo) {
+      max = array[index].codigo;
     }
   }
 
   return max;
 }
-
 
 int CountingSort(T *array, const size_t size, int exponent) {
 
@@ -38,10 +37,10 @@ int CountingSort(T *array, const size_t size, int exponent) {
     return ENOMEM;
   }
 
-  memset(counting,0,DECIMAL * sizeof(T));
+  memset(counting, 0, DECIMAL * sizeof(T));
 
   for (size_t index = 0; index < size; ++index) {
-    ++counting[DIGIT(array[index], exponent)];
+    ++counting[DIGIT(array[index].codigo, exponent)];
   }
 
   unsigned accumulation = 0;
@@ -55,23 +54,23 @@ int CountingSort(T *array, const size_t size, int exponent) {
   }
 
   for (size_t index = 0; index < size; ++index) {
-    buffer[counting[DIGIT(array[index], exponent)]++] = array[index];
+    buffer[counting[DIGIT(array[index].codigo, exponent)]++] = array[index];
   }
 
   memcpy(array, buffer, size * sizeof(T));
 
   free(buffer);
+
   exponent *= DECIMAL;
 
   return EXIT_SUCCESS;
 }
 
-
 int RadixSortLSD(T *array, const size_t size) {
-  T max = GetMax(array, size);
+  int max = GetMax(array, size);
 
   for (int exponent = 1; (max / exponent) > 0; exponent *= DECIMAL) {
-    assert(CountingSort(array, size,exponent) == EXIT_SUCCESS);
+    assert(CountingSort(array, size, exponent) == EXIT_SUCCESS);
   }
 
   return EXIT_SUCCESS;
